780db7d222b2acb4f39495ecb037ca6780d44649
VorobevEvg <159680182+VorobevEvg@users.noreply.github.com>
2024-05-22 18:00:27 +0400

    Update Main.java

--- ItogTask2/src/Main.java
+++ ItogTask2/src/Main.java
@@ -5,16 +5,27 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите уравнение:");
-        String str =in.nextLine();
-        //str.matches(".*\\d.*");
-        System.out.println("Вывод: "+str.matches(".*\\d.*"));
-        /*String [] result = str.split("=");
-        String [] result1 = str.split("\\+");
-        String res1=result[0];
-        String res2=result[1];
-        System.out.println("Вывод: "+res2);*/
+        // String array[] = new String[4];
+        System.out.println("Ввод:");
+        String str = in.next();
+        char[] charArray = str.toCharArray();
+        int result = 0;
+        int x1 = Character.digit(charArray[0], 10);
+        int x2 = Character.digit(charArray[2], 10);
+        int x3 = Character.digit(charArray[4], 10);
+        if (charArray[1] == '-') {
+            if (charArray[0] == 'x') {
+                result = x2 + x3;
+            }
+            if (charArray[2] == 'x') {
+                result = x1 - x3;
+            }
+            if (charArray[4] == 'x') {
+                result = x1 - x2;
+            }
+               System.out.println("Вывод: "+result);
 
+        }
     }
 }
 
