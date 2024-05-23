33b684aa0638de24dba6f979fb1dbae7c5e6eeee
VorobevEvg <159680182+VorobevEvg@users.noreply.github.com>
2024-05-23 16:14:55 +0400

    Update Main.java

--- Task13/src/Main.java
+++ Task13/src/Main.java
@@ -1,10 +1,22 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите строку:");
-        String length =in.next();
-
+        String length =in.nextLine();
+        String [] result = length.split(" ");
+        int count=0;
+        for (int i=0; i < result.length; i++)
+        {
+            if (Pattern.matches("[a-zA-Z]+",result[i])==true)
+            {
+                System.out.println(result[i]);
+                count=count+1;
+            }
+        }
+        System.out.println("Кол-во: "+ count);
     }
 }
